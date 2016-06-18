e477f2125da3354a89a5b0f1696e86fb228fce3b
William Hermans <yyrkoon@gmail.com>
2016-06-17 20:52:59 -0700

    Added new example led-example.js

---
+++ led-example.js
@@ -0,0 +1,44 @@
+"use strict";
+
+(function() {
+        var led = require('./usr-leds.js');
+        var timerID = 0;
+        var stdin = process.stdin;
+        stdin.setRawMode( true );
+        stdin.setEncoding( 'utf8' );
+        
+        // Turn off all USR LEDs
+        for(var n=0; n < 4; n++){
+            led.write(n, 'trigger', 'none');
+        }
+        
+        // Create a blink pattern
+        var led_number = 0;
+        var led_value = 0;
+        
+        function toggleLED(value){
+                
+                if(led_number <= 3){
+                        led.write(led_number, 'brightness', led_value);
+                        led_number++;
+                }else{
+                        led_number = 0;
+                        led_value ^= 1;
+                }
+        };
+        
+        timerID = setInterval(toggleLED, 100);
+        
+        stdin.on( 'data', function( key ){
+                // ctrl-c ( end of text )
+                if ( key === '\u0003' ) {
+                        process.stdout.write('Caught CTRL + C keypress, turning off LEDs . . .');
+                        clearInterval(timerID);
+                        for(var n=0; n < 4; n++){
+                                led.write(n, 'brightness', 0);
+                        }
+                        process.stdout.write(' Exiting.' + '\n');
+                        process.exit();
+                }
+        });              
+})();
